e44b8cc052f8952b33a9ba101798b406c41cbb40
Diego Rebora <bogacore@hotmail.com>
2023-05-20 18:39:57 -0300

    En la rama modificados los modelos, ya tenemos el modelo comentario y los modelos con su relacion con el modelo comentario, no se rompió

--- PadelApp/models.py
+++ PadelApp/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.contenttypes.models import ContentType
+
+
 
 # Create your models here.
+class Comentario(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    objeto_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    objeto_id = models.PositiveIntegerField()
+    objeto = GenericForeignKey('objeto_type', 'objeto_id')
+    texto = models.TextField()
+    titulo = models.CharField(max_length=100)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.texto
 class Jugadores(models.Model):
     nombre =  models.CharField(max_length=64)
     apellido =  models.CharField(max_length=64)
@@ -10,7 +28,7 @@ class Jugadores(models.Model):
     circuito = models.CharField(max_length=64)
     marca = models.CharField(max_length=64, blank=True)
     creador = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-
+    comentarios = GenericRelation(Comentario, related_query_name='Jugadores_comentarios')
     def __str__(self):
         return f"{self.nombre}, {self.apellido}"
 
@@ -20,7 +38,7 @@ class Circuito(models.Model):
     ranking_premios = models.IntegerField()
     cant_torneos = models.IntegerField(null=True, blank=True)
     creador = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    
+    comentarios = GenericRelation(Comentario, related_query_name='Circuito_comentarios')
     
     def __str__(self):
         return f"{self.nombre} | {self.alcance}"
@@ -30,7 +48,20 @@ class Marca(models.Model):
     nombre = models.CharField(max_length=64)
     origen = models.CharField(max_length=64)
     creador = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-
+    comentarios = GenericRelation(Comentario, related_query_name='Marca_comentarios')
+    
     def __str__(self):
         return f"{self.nombre}"
  
+class Comentario(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    objeto_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    objeto_id = models.PositiveIntegerField()
+    objeto = GenericForeignKey('objeto_type', 'objeto_id')
+    texto = models.TextField()
+    titulo = models.CharField(max_length=100)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.texto

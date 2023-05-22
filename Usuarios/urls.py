25db60b57ec53554868e82d2a7c85e7a859a8174
Diego Rebora <bogacore@hotmail.com>
2023-05-22 14:06:15 -0300

    Agregado editar perfil de usuario y menu desplegable

--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from Usuarios.views import registro
-from Usuarios.views import login_view, CustomLogoutView
+from Usuarios.views import login_view, CustomLogoutView, MiPerfilUpdateView
 
     
     
@@ -9,5 +9,6 @@ urlpatterns = [
     path("registro/", registro, name="registro" ),
     path("login/", login_view, name="login" ),
     path("logout/", CustomLogoutView.as_view(), name="logout" ),
+    path("editar_usuario/", MiPerfilUpdateView.as_view(), name="editar_usuario" ),
 
 ]
--- Usuarios/views.py
+++ Usuarios/views.py
@@ -1,6 +1,10 @@
 
 from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import UpdateView
+
+from Usuarios.forms import UserRegisterForm, UserUpdateForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LogoutView
@@ -53,4 +57,12 @@ def login_view(request):
 
 class CustomLogoutView(LogoutView):
    template_name = 'Usuarios/logout.html'
+
+class MiPerfilUpdateView(LoginRequiredMixin, UpdateView):
+   form_class = UserUpdateForm
+   success_url = reverse_lazy('inicio')
+   template_name = 'Usuarios/formulario_perfil.html' ##RECORDAR CAMBIAR ESTE TEMPLATE
+
+   def get_object(self, queryset=None):
+       return self.request.user
 
